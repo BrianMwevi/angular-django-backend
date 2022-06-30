c39ab768af7bedfe19c80a94ce6768daadb6f925
brianmwevi <mwevibrian@gmail.com>
2022-06-30 22:14:14 +0300

    docs: add dummy var in cloudinary

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-python manage.py makemigrations
-python manage.py migrate
+# python manage.py makemigrations
+# python manage.py migrate
 
-heroku config:set API_KEY=2187627981sds19646
-heroku config:set API_SECRET=OqsHrsdswFb-l8xmD_9eUcYBj_tVN8
-heroku config:set CLOUDINARY_URL=cloudinary://1233349796sdfsd96849:Jvd78nFKUziejHAW-b5Qn0gLuHk@da7srpwm6
-heroku config:set CLOUD_NAME=sdsd
+# heroku config:set API_KEY=2187sdfsdf627981sds19646
+# heroku config:set API_SECRET=sdfsdfs-l8xmD_9eUcYBj_tVN8
+# heroku config:set CLOUDINARY_URL=cloudinary://sdfsdfsdfsdf:Jvd7sdfsd0gLuHk@da7srpwm6
+# heroku config:set CLOUD_NAME=sdsdsdfsdf
 
-heroku config:set DEBUG=False
-heroku config:set DISABLE_COLLECTSTATIC=1
-heroku config:set MODE=prod
-heroku config:set SECRET_KEY='django-insecure-h-4_vq % @6x462t8ly = k = +8os_54n7_lziad!i4 *$_rey9b@1mb'
-heroku config:set ALLOWED_HOSTS=demo-angular-django.herokuapp.com
+# heroku config:set DEBUG=False
+# heroku config:set DISABLE_COLLECTSTATIC=1
+# heroku config:set MODE=prod
+# heroku config:set SECRET_KEY='django-insecure-h-4_vq % @6x462t8ly = k = +8os_54n7_lziad!i4 *$_rey9b@1mb'
+heroku config:set ALLOWED_HOSTS=angular-django.herokuapp.com
 
 
 git switch -c main
@@ -30,7 +30,7 @@ git push heroku main
 # heroku run python manage.py makemigrations
 # heroku run python manage.py migrate
 # heroku pg:reset
-# heroku pg:push eloan DATABASE_URL --app eloan-ke
+# heroku pg:push demo11 DATABASE_URL --app angular-django
 
 git switch develop
 heroku open
